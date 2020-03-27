65bde2dc78c5080a9bb056c37c42139087f4bf88
AnBrusn <brusnitsyna.an@yandex.ru>
2020-03-27 14:00:44 +0300

    Add goroutines

--- createIndexFromFiles/main.go
+++ createIndexFromFiles/main.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"regexp"
+	"sync"
 
 	"github.com/polisgo2020/search-AnBrusn/index"
 )
@@ -32,20 +34,66 @@ func writeInvertedIndex(outputFile string, invertedIndexes index.Index) error {
 	return nil
 }
 
-func readFromDirectory(dirname string) (map[string]string, error) {
+func readFile(path string, filename string, dataChan chan<- [2]string, errChan chan<- error,
+	wg *sync.WaitGroup, mutex *sync.Mutex) {
+	defer wg.Done()
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		errChan <- err
+	}
+	re := regexp.MustCompile(`[^\w]+`)
+	words := re.Split(string(data), -1)
+	for _, word := range words {
+		mutex.Lock()
+		dataChan <- [2]string{word, filename}
+		mutex.Unlock()
+	}
+}
+
+func createFromDirectory(dirname string) (index.Index, error) {
+	invertedIndex := make(index.Index)
 	files, err := ioutil.ReadDir(dirname)
 	if err != nil {
-		return nil, err
+		return invertedIndex, err
 	}
-	dataFromFiles := make(map[string]string)
+
+	errChan := make(chan error)
+	dataChan := make(chan [2]string)
+	mutex := &sync.Mutex{}
+
+	wgForListener := &sync.WaitGroup{}
+	wgForListener.Add(1)
+	go listener(invertedIndex, dataChan, errChan, wgForListener)
+
+	wgForFilesReading := &sync.WaitGroup{}
+	wgForFilesReading.Add(len(files))
+	go func() {
+		wgForFilesReading.Wait()
+		close(dataChan)
+	}()
+
 	for _, currentFile := range files {
-		data, err := ioutil.ReadFile(filepath.Join(dirname, currentFile.Name()))
-		if err != nil {
-			return nil, err
-		}
-		dataFromFiles[currentFile.Name()] = string(data)
+		path := filepath.Join(dirname, currentFile.Name())
+		go readFile(path, currentFile.Name(), dataChan, errChan, wgForFilesReading, mutex)
 	}
-	return dataFromFiles, nil
+
+	wgForListener.Wait()
+	return invertedIndex, nil
+}
+
+func listener(invertedIndex index.Index, dataChan <-chan [2]string, errChan <-chan error, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for {
+		select {
+		case <-errChan:
+			return
+		case wordInfo, ok := <-dataChan:
+			if !ok {
+				return
+			}
+			invertedIndex.AddToken(wordInfo[0], wordInfo[1])
+		}
+	}
 }
 
 func main() {
@@ -53,11 +101,7 @@ func main() {
 		log.Fatal("There must be 2 arguments: path to the folder with input files and output file path")
 	}
 
-	data, err := readFromDirectory(os.Args[1])
-	if err != nil {
-		log.Fatal(err)
-	}
-	invertedIndex, err := index.CreateInvertedIndex(data)
+	invertedIndex, err := createFromDirectory(os.Args[1])
 	if err != nil {
 		log.Fatal(err)
 	}
--- index/createIndex.go
+++ index/createIndex.go
@@ -1,8 +1,6 @@
 package index
 
 import (
-	"regexp"
-
 	"github.com/bbalet/stopwords"
 	"github.com/kljensen/snowball"
 )
@@ -14,31 +12,24 @@ type FileWithFreq struct {
 
 type Index map[string][]FileWithFreq
 
-func CreateInvertedIndex(data map[string]string) (Index, error) {
-	var invertedIndexes = make(Index)
-	re := regexp.MustCompile(`[^\w]+`)
-	for fileName, fileData := range data {
-		wordsInCurrentFile := re.Split(fileData, -1)
-		for _, word := range wordsInCurrentFile {
-			stemWord := stopwords.CleanString(word, "en", true)
-			if len(stemWord) > 0 {
-				stemWord, err := snowball.Stem(stemWord, "english", true)
-				if err != nil {
-					return nil, err
-				}
-				isFound := false
-				for i, el := range invertedIndexes[stemWord] {
-					if el.Filename == fileName {
-						invertedIndexes[stemWord][i].Freq++
-						isFound = true
-						break
-					}
-				}
-				if !isFound {
-					invertedIndexes[stemWord] = append(invertedIndexes[stemWord], FileWithFreq{fileName, 1})
-				}
+func (index Index) AddToken(word string, filename string) error {
+	stemWord := stopwords.CleanString(word, "en", true)
+	if len(stemWord) > 0 {
+		stemWord, err := snowball.Stem(stemWord, "english", true)
+		if err != nil {
+			return err
+		}
+		isFound := false
+		for i, el := range index[stemWord] {
+			if el.Filename == filename {
+				index[stemWord][i].Freq++
+				isFound = true
+				break
 			}
 		}
+		if !isFound {
+			index[stemWord] = append(index[stemWord], FileWithFreq{filename, 1})
+		}
 	}
-	return invertedIndexes, nil
+	return nil
 }
